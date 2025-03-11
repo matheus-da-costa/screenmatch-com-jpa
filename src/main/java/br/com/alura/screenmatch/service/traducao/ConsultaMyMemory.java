218b88f5a968903644f368949a8e74d6318437c5
Matheus <matheusdacsousa@gmail.com>
2025-03-11 14:01:38 -0400

    feat - Estudo da persistencia de dados com spring

---
+++ src/main/java/br/com/alura/screenmatch/service/traducao/ConsultaMyMemory.java
@@ -0,0 +1,37 @@
+package br.com.alura.screenmatch.service.traducao;
+
+import java.net.URLEncoder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+public class ConsultaMyMemory {
+
+    public static String obterTraducao(String text) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        ConsumoApi consumo = new ConsumoApi();
+
+        @SuppressWarnings("deprecation")
+        String texto = URLEncoder.encode(text)
+        
+        ;
+        @SuppressWarnings("deprecation")
+        String langpair = URLEncoder.encode("en|pt-br");
+
+        String url = "https://api.mymemory.translated.net/get?q=" + texto + "&langpair=" + langpair;
+
+        String json = consumo.obterDados(url);
+
+        DadosTraducao traducao;
+        try {
+            traducao = mapper.readValue(json, DadosTraducao.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return traducao.dadosResposta().textoTraduzido();
+    }
+}
